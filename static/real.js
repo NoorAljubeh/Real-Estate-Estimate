ffa1f102b8739d9f625fdcd3c62daadcda97843a
ahmed deria <ahmedderia0@gmail.com>
2023-09-01 15:58:28 +0300

    Fix flask server and connect the frontend with the backend

--- static/real.js
+++ static/real.js
@@ -1,48 +1,80 @@
 // For .ml3 animation
-var textWrapper = document.querySelector('.ml3');
+var textWrapper = document.querySelector(".ml3");
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml3 .letter',
-    opacity: [0, 1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i + 1)
-  }).add({
-    targets: '.ml3',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+const predictionResult = document.querySelector(".prediction-result");
+const featuresForm = document.querySelector("form");
+
+featuresForm.addEventListener("submit", e => {
+    e.preventDefault();
+    let data = new FormData(e.currentTarget);
+    data = Object.fromEntries(data.entries());
+    for (key in data) {
+        data[key] = Number(data[key]);
+    }
+
+    fetch("/predict_price", {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+        method: "POST"
+    })
+        .then(data => data.json())
+        .then(predicted_price => {
+            console.log(predicted_price);
+            predictionResult.textContent = `Predicted Price: $${predicted_price.value}`;
+        })
+        .catch(err => {
+            predictionResult.textContent = `...`;
+            console.log(err);
+        });
+});
+
+anime
+    .timeline({ loop: true })
+    .add({
+        targets: ".ml3 .letter",
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i + 1)
+    })
+    .add({
+        targets: ".ml3",
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
 
 // For .ml2 animation
-var textWrapper2 = document.querySelector('.ml2');
+var textWrapper2 = document.querySelector(".ml2");
 textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml2 .letter',
-    scale: [4, 1],
-    opacity: [0, 1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: (el, i) => 70 * i
-  }).add({
-    targets: '.ml2',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+anime
+    .timeline({ loop: true })
+    .add({
+        targets: ".ml2 .letter",
+        scale: [4, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70 * i
+    })
+    .add({
+        targets: ".ml2",
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
 
-
-  const slider = document.querySelector('.slider');
-const sliderItems = document.querySelectorAll('.care');
-const leftButton = document.querySelector('.left-button');
-const rightButton = document.querySelector('.right-button');
+const slider = document.querySelector(".slider");
+const sliderItems = document.querySelectorAll(".care");
+const leftButton = document.querySelector(".left-button");
+const rightButton = document.querySelector(".right-button");
 
 let currentIndex = 0;
 
@@ -56,25 +88,12 @@ function slideTo(index) {
     slider.style.transform = `translateX(${offset}px)`;
 }
 
-leftButton.addEventListener('click', () => {
+leftButton.addEventListener("click", () => {
     slideTo(currentIndex - 1);
 });
 
-rightButton.addEventListener('click', () => {
+rightButton.addEventListener("click", () => {
     slideTo(currentIndex + 1);
 });
 
 slideTo(currentIndex); // Initial slide
-
-
-const refreshButton = document.getElementById('refreshButton');
-    const predictionElement = document.getElementById('prediction');
-
-    refreshButton.addEventListener('click', async () => {
-        // Perform an AJAX request to update the prediction
-        const response = await fetch('/get_prediction', { method: 'GET' });
-        const data = await response.json();
-        
-        // Update the prediction element with the new prediction value
-        predictionElement.textContent = data.prediction;
-    });
